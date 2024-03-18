f0a0c37d5d247cf0bb7ff3f8fe61a7bf394d939d
nadox00 <162769750+nadox00@users.noreply.github.com>
2024-03-18 19:33:37 -0300

    Add files via upload
    Um dos projetos que eu fiz na aula da faculdade e esqueci de subir pro meu github

---
+++ Empresa.java
@@ -0,0 +1,17 @@
+public class Empresa {
+    
+    public static void main (String[] args) {
+        int jan = 15000;
+        int fev = 17000;
+        int mar = 23000;
+
+    int soma = jan + fev + mar ;
+    int media = jan + fev + mar /3;
+
+    System.out.print("O gasto total foi de: " + soma);
+    System.out.print("Media de gasto foi: " + media);
+
+
+    }
+    
+}
