88547fd520fb524411876a70b1e30feb7dccfdbd
nadox00 <162769750+nadox00@users.noreply.github.com>
2024-03-18 19:36:04 -0300

    Delete tempCforF.java

--- tempCforF.java
+++
@@ -1,9 +0,0 @@
-public class tempCforF {
-    
-    public static void main (String[] args) {
-        float celsius = 30f;
-        double result = ((9*celsius)/5) +32;
-
-        System.out.print(result);
-    }
-}
