2bc30346b45adabd9e178ffbf85eca29048d60ff
nadox00 <162769750+nadox00@users.noreply.github.com>
2024-03-18 19:36:12 -0300

    Delete tempFforC.java

--- tempFforC.java
+++
@@ -1,9 +0,0 @@
-
-public class tempFforC {
-    public static void main (String[] args) {
-        float tempF = 86f;
-        double result = ((tempF-32)*5/9);
-
-        System.out.print(result);
-    }
-}
