a482710a923f2dd4e06d7e0aa9a4f249496e149f
nadox00 <nadoxft26@gmail.com>
2024-03-23 19:15:42 -0300

    Exercicio 09H Resolvido

--- Exercicio09H.java
+++ Exercicio09H.java
@@ -5,22 +5,56 @@ public class Exercicio09H {
         @SuppressWarnings ("resource")
         
         Scanner teclado = new Scanner (System.in);
+        System.out.println("|-------------------------|");
+        System.out.println("    COMPRA DE INGRESSOS    ");
+        System.out.println("|-------------------------|");
+
         System.out.println("Insira o dia da semana");
         String dia = teclado.nextLine();        
-        teclado.nextLine();
-        System.out.println("Qual sua idade ?");
-        int idade = teclado.nextInt();
-        System.out.println("Possui carteirinha de meia entrada ?");
-        String sORn = teclado.nextLine();
 
-        String[] diaSemana = {
-            "Segunda", "Terça", "Quarta", "Quinta",
-            "Sexta", "Sabado", "Domingo"
-        };
-        
-        int valorDoIngresso = 0;
+        System.out.println("Possui carteirinha de meia entrada ou é menor de 18 anos? ");
+        String carteirinha = teclado.nextLine();
 
+        double valor1 = 20;
+        double valor2 = 16;
+        double valor3 = 26;
+        double valorDoIngresso;
+        double valorDoIngressoComDesconto;
 
-    }
-    
+    if ( dia.equalsIgnoreCase("Segunda") || dia.equalsIgnoreCase("Terça") || dia.equalsIgnoreCase("Quinta")) {
+        valorDoIngresso = valor1;
+        valorDoIngressoComDesconto = valor1 /2;
+        double preçofinal = carteirinha.equals("Sim") ? valorDoIngressoComDesconto : valor1; 
+        System.out.println("|---------------|");
+        System.out.println("DADOS DO INGRESSO");
+        System.out.println("|---------------|");
+        System.out.println("Dia da semana: " +dia);
+        System.out.println("Possui carteirinha ou é menor de 18 anos ? " +carteirinha);
+        System.out.println("Preço Normal " +valorDoIngresso);
+        System.out.println("Preço com desconto " +preçofinal);
+    } else if ( dia.equals("Quarta")) {
+        valorDoIngresso = valor2;
+        valorDoIngressoComDesconto = valor2 /2;
+        double preçofinal = carteirinha.equals("Sim") ? valorDoIngressoComDesconto : valor2; 
+        System.out.println("|---------------|");
+        System.out.println("DADOS DO INGRESSO");
+        System.out.println("|---------------|");
+        System.out.println("Dia da semana: " +dia);
+        System.out.println("Possui carteirinha ou é menor de 18 anos ? " +carteirinha);
+        System.out.println("Preço Normal " +valorDoIngresso);
+        System.out.println("Preço com desconto " +preçofinal);
+    } else if ( dia.equals("Sexta") || dia.equals("Sabado") || dia.equals("Domingo")) {
+        valorDoIngresso = valor3;
+        valorDoIngressoComDesconto = valor3 /2;
+        double preçofinal = carteirinha.equals("Sim") ? valorDoIngressoComDesconto : valor3; 
+        System.out.println("|---------------|");
+        System.out.println("DADOS DO INGRESSO");
+        System.out.println("|---------------|");
+        System.out.println("Dia da semana: " +dia);
+        System.out.println("Possui carteirinha ou é menor de 18 anos ? " +carteirinha);
+        System.out.println("Preço Normal " +valorDoIngresso);
+        System.out.println("Preço com desconto " +preçofinal);
+     } 
+    } 
 }
+// Consegui resolver esse exercicio, penei bastante mas tô feliz de ter conseguido resolver.
