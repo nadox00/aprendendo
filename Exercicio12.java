9021c0fe739bdb509eb26cb1e697e7edf5d305a6
nadox00 <162769750+nadox00@users.noreply.github.com>
2024-03-25 21:41:57 -0300

    Update Exercicio12.java
    Possuia um erro nesse exercicio que foi corrigido

--- Exercicio12.java
+++ Exercicio12.java
@@ -4,31 +4,31 @@ public class Exercicio12 {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         System.out.println("Digite um numero");
-        int numero1 = teclado.nextInt();
+        double numero1 = teclado.nextDouble();
         teclado.nextLine();
         System.out.println("Digite o simbolo de operacão");
         String simbolo = teclado.nextLine();
         System.out.println("Digite outro numero");
-        int numero2 = teclado.nextInt();
-        int resultadomais = numero1 + numero2;
-        int resultadomenos = numero1 - numero2;
-        int resultadovezes = numero1 * numero2;
-        int resultadodividido = numero1 / numero2;
+        double numero2 = teclado.nextDouble();
 
         switch (simbolo) {
             case "+":
-            System.out.println("Resultado " +resultadomais);
+            System.out.println("Resultado " + (numero1+numero2));
             break;
             case "-":
-            System.out.println("Resultado " +resultadomenos);
+            System.out.println("Resultado " + (numero1-numero2));
             break;
             case "*":
-            System.out.println("Resultado " +resultadovezes);
+            System.out.println("Resultado " + (numero1*numero2));
             break;
             case "/":
-            System.out.println("Resultado " +resultadodividido);
+            if (numero2 == 0) {
+                System.out.println("Não é possivel dividir por 0");
+            } else {
+            System.out.println("Resultado " + (numero1/numero2));
             break;
         }
     }
-    
 }
+}
+
