1de62afbdb270a35035fc623171515ae0b7dc123
nadox00 <nadoxft26@gmail.com>
2024-03-27 19:01:55 -0300

    Exercicio concluido

--- Exercicio13_4.java
+++ Exercicio13_4.java
@@ -2,13 +2,19 @@ import java.util.Scanner;
 
 public class Exercicio13_4 {
     public static void main(String[] args) {
+        @SuppressWarnings("resource")
         Scanner teclado = new Scanner(System.in);
         
-        for (int i = 1; i < 5; i++) {
+        int idade = 0;
+        int maior = 0;
+        for (int i = 1; i <= 5; i++) {
             System.out.println("Quais são as idades ? ");
-            teclado.nextInt();
-            
-            
+            idade = teclado.nextInt();
+
+            if (idade >= 18) {
+                maior++;
+            }
     }
+    System.out.println("O numero de maiores são " +maior);
 }
 }
