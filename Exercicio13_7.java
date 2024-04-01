2475feb3aff32e177367d75d5d6171c0c0d827b4
nadox00 <nadoxft26@gmail.com>
2024-04-01 20:55:17 -0300

    Exercicio Dia 01/04

---
+++ Exercicio13_7.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Exercicio13_7 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite um nome");
+        String nome = teclado.nextLine();
+
+        for ( int i = 0; i < nome.length(); i ++) {
+            System.out.println(nome.charAt(i));
+        }
+    }
+    
+}
+// Fiz tudo olhando pro chatgpt.
